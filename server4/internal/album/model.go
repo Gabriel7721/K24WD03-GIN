851b2f6f198bc14b91c47e72ae78e7849d41a973
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-20 15:34:37 +0700

    update

--- server4/internal/album/model.go
+++ server4/internal/album/model.go
@@ -18,3 +18,38 @@ var albums = []album{
 	{ID: "2", Title: "Back in Black", Artist: "AC/DC", Price: 2500.99, OwnerID: "u2"},
 	{ID: "3", Title: "The Bodyguard", Artist: "Whitney Houston", Price: 3500.99, OwnerID: "u2"},
 }
+
+func GetAll() []album {
+	return albums
+}
+func GetOne(id string) *album {
+	for i := range albums {
+		if albums[i].ID == id {
+			return &albums[i]
+		}
+	}
+	return nil
+}
+func CreateOne(a album) album {
+	albums = append(albums, a)
+	return a
+}
+func UpdateOne(id string, updated album) (*album, bool) {
+	for i := range albums {
+		if albums[i].ID == id {
+			updated.ID = id
+			albums[i] = updated
+			return &albums[i], true
+		}
+	}
+	return nil, false
+}
+func DeleteOne(id string) bool {
+	for i := range albums {
+		if albums[i].ID == id {
+			albums = append(albums[:i], albums[i+1:]...)
+			return true
+		}
+	}
+	return false
+}
