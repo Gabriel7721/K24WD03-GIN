6e14b32d218d939da9ab324f7bc1dd79bedae696
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-20 15:53:23 +0700

    update jwt

--- server4/internal/auth/jwt.go
+++ server4/internal/auth/jwt.go
@@ -1,1 +1,42 @@
 package auth
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtSecret = []byte("PWRVXYGkp3cwNvrR4wymoAX8EnU1XkEf3MFnNwzp/2WFhPGPMjx48w8ZBgfv4N+ADdbJh0TbH9t8ANfGZ9ZyGw==")
+
+type Claims struct {
+	UserID   string `json:"user_id"`
+	Username string `json:"username"`
+	Role     string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(userID, username, role string) (string, error) {
+	claims := Claims{
+		UserID:   userID,
+		Username: username,
+		Role:     role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
+	return token.SignedString(jwtSecret)
+}
+func ParseToken(tokenStr string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, &Claims{}, func(t *jwt.Token) (any, error) {
+		return jwtSecret, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, jwt.ErrTokenInvalidClaims
+}
