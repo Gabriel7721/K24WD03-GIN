20c2a2ecbc2528fe2a4e19d40b4b8528f87bb678
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-20 16:52:32 +0700

    update

--- server4/main.go
+++ server4/main.go
@@ -15,6 +15,17 @@ func main() {
 	api := router.Group("/api")
 	api.Use(auth.AuthMiddleware())
 
+	// NOTE: Homeworks 1
+	api.GET("/albums")
+	api.GET("/albums/:id")
+	api.POST("/albums")
+	api.PUT("/albums/:id")
+	api.PATCH("/albums/:id")
+	// ====
+
+	// NOTE: Homeworks 2
+	// Kết nối với MySQL/MongoDB
+
 	api.DELETE("/albums/:id", album.DeleteAlbumHandler)
 
 	router.Run(":9999")
