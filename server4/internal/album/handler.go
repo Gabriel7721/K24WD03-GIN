063c0342c9aac7789b2d813839241acbecee990e
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-20 16:31:25 +0700

    update delete

--- server4/internal/album/handler.go
+++ server4/internal/album/handler.go
@@ -1,1 +1,52 @@
 package album
+
+import (
+	"fmt"
+	"net/http"
+	"server4/internal/auth"
+	"server4/internal/user"
+
+	"github.com/gin-gonic/gin"
+)
+
+var lastID = 3
+
+func GenerateAlbumID() string {
+	lastID++
+	return fmt.Sprintf("%d", lastID)
+}
+
+func EnSureArtistOwnerID(ctx *gin.Context, a *album) bool {
+	role := auth.GetRole(ctx)
+	userID := auth.GetUserID(ctx)
+	if role != string(user.RoleArtist) {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"msg": "Only Artists could access"})
+		return false
+	}
+	if a.OwnerID != userID {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"msg": "It is not belong to you"})
+		return false
+	}
+	return true
+}
+
+func DeleteAlbumHandler(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	existing := GetOne(id)
+
+	if existing == nil {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"msg": "not found"})
+		return
+	}
+
+	if !EnSureArtistOwnerID(ctx, existing) {
+		return
+	}
+
+	if !DeleteOne(id) {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"msg": "can not delete album"})
+		return
+	}
+	ctx.Status(http.StatusNoContent)
+}
